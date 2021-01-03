e30fd1387f893c99c15fb0343394752c8ad77348
e-emd <dewin.emd@gmail.com>
2021-01-03 09:51:05 -0700

    Counter is working this was pratice for redux

---
+++ src/reducers/combineReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import counterReducer from './counter';
+import loggedReducer from './logIn';
+
+const allReducers = combineReducers({
+  counter: counterReducer,
+  isLogged: loggedReducer,
+});
+
+export default allReducers;
